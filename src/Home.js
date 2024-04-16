8d98ee4df88c0d7eff315ecfe7355881e762988f
Tabrez Khan <khan.t.29@gmail.com>
2024-04-16 16:35:34 +0200

    fetch device uid using firebolt sdk

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 import { Lightning, Router, Colors } from "@lightningjs/sdk";
+import { Device, Lifecycle } from "@firebolt-js/sdk";
 
 export default class Home extends Lightning.Component {
   static _template() {
     return {
-      Background: {
-        rect: true,
-        w: 1920,
-        h: 1080,
+      rect: true,
+      w: 1920,
+      h: 1080,
+      color: 0xff000000,
+      Uid: {
         color: Colors("red").get(),
         text: {
-          text: "Home page",
+          text: "Getting device uid...",
           textColor: 0xffffffff,
           fontFace: "Regular",
           fontSize: 48,
@@ -18,8 +20,19 @@ export default class Home extends Lightning.Component {
     };
   }
 
+  async _enable() {
+    Lifecycle.ready();
+    const uid = await Device.uid();
+    console.log("UID: ", uid);
+    this.tag("Uid").patch({
+      text: {
+        text: uid,
+      },
+    });
+  }
+
   _getFocused() {
-    this.tag("Background");
+    this;
   }
 
   _handleBack() {
